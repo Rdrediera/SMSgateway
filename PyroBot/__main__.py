a6b7c7314f10482058cbbe4329b8d8ef254141e4
iam-NVN <fakecreditcard88@gmail.com>
2021-11-13 15:14:19 +0530

    vs code

---
+++ PyroBot/__main__.py
@@ -0,0 +1,115 @@
+import os
+import logging
+import pyrogram
+from decouple import config
+import mysql.connector, requests
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# vars
+APP_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+AUTH_IDS = set(int(x) for x in os.environ.get("AUTH_IDS").split())
+ADMIN_IDS = set(int(x) for x in os.environ.get("ADMIN_IDS").split())
+
+
+TWILIO_SID = "ACf15c73e78baba374db270f259ded078c"
+TWILIO_AUTHTOKEN = "c5495026c2b915f875aa4c1004e77de2"
+TWILIO_SERVICEID = "MG962b4acdc6e5cf23b4e601d61e87b350"
+DEDUCTION_PER_SEND = 0.02
+AMOUNT_PER_CREDIT = 0.02
+
+
+mydb = mysql.connector.connect(
+  host="sql3.freesqldatabase.com",
+  user="sql3450594",
+  password="65DvDAPiGJ",
+  database="sql3450594"
+)
+mycursor = mydb.cursor()
+
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
+      
+def isPremium(userid):
+  sql = f"SELECT * FROM premium WHERE userid = {userid}"
+  resp = mycursor.execute(sql)
+  result = mycursor.fetchall()
+  if result:
+    return True
+  else:
+    return False
+
+def addPremium(userid,credits):
+    if isPremium(userid) != True:
+        sql = f"INSERT INTO premium (userid, credits) VALUES ('{userid}','{float(credits)}')"
+        mycursor.execute(sql)
+        mydb.commit()
+        return True
+    elif isPremium(userid) == True:
+        currentcredits = getCredits(userid)
+        newcredits = num(currentcredits)+num(credits)
+        
+        sql = f"UPDATE premium SET credits = '{newcredits}' WHERE userid = '{userid}'"
+        mycursor.execute(sql)
+        mydb.commit()
+        return True
+        
+
+def banPremium(userid):
+  sql = f"DELETE FROM premium WHERE userid='{userid}'"
+  mycursor.execute(sql)
+  mydb.commit()
+  return True
+
+def getCredits(userid):
+  sql = f"SELECT credits FROM premium WHERE userid = '{userid}'"
+  mycursor.execute(sql)
+  result = mycursor.fetchall()
+  return result[0][0]
+
+def deductCredits(userid,credits):
+  currentcredits = getCredits(userid)
+  newcredits = num(currentcredits)-num(credits)
+  
+  sql = f"UPDATE premium SET credits = '{newcredits}' WHERE userid = '{userid}'"
+  mycursor.execute(sql)
+  mydb.commit()
+  
+async def sendSMS(number,smsmessage):
+    smsmessage = smsmessage.replace('{number}',number)
+    
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    postdata = {
+
+    'To': number,
+    'Body': smsmessage,
+    'MessagingServiceSid': TWILIO_SERVICEID
+
+    }
+    postsms = requests.post(f'https://api.twilio.com/2010-04-01/Accounts/{TWILIO_SID}/Messages.json', headers=headers, data=postdata, auth=(TWILIO_SID, TWILIO_AUTHTOKEN))
+    
+    if postsms.json()['status'] == 'accepted':
+        return True
+    else:
+        return False
+  
+
+if __name__ == "__main__" :
+    print("Starting Bot...")
+    plugins = dict(root="PyroBot/plugins")
+    app = pyrogram.Client(
+        "Ninja",
+        bot_token=BOT_TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+    app.run()
+    
+    
---
+++ PyroBot/plugins/start.py
@@ -0,0 +1,210 @@
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
+import requests
+from time import sleep
+import os
+
+from requests.api import head
+from __main__ import *
+
+
+@Client.on_message(filters.command(["start"]))
+async def startcmd(client:Client, message:Message):
+    user = message.from_user or message.sender_chat
+    user_mention = user.mention if message.from_user else user.title  
+    await message.reply_text(f'**Hello** {user_mention}, \n\n**Type /cmds to know all my commands!**', 
+                  reply_markup=InlineKeyboardMarkup([
+                               [InlineKeyboardButton("💠 Developer", url="t.me/ninjanaveen")],
+                               [InlineKeyboardButton("💰 Buy Authorization", url="t.me/KING_ANONYMOUS_XD")]]))
+       
+@Client.on_message(filters.command(["cmds"]))
+async def ook(_, m):
+    await m.reply("**Which commands would you like to check?**", 
+                  reply_markup=InlineKeyboardMarkup([
+                               [InlineKeyboardButton("☎️ SMS Gateway Commands", f"help_gateway")]]))
+
+    
+@Client.on_callback_query(filters.regex(r"^help_.*"))
+async def cbstart(_, query: CallbackQuery):
+    data = query.data
+    sp = data.split("_")[1]
+    user_id = query.from_user.id
+    
+    
+    if sp == "gateway":
+        if not isPremium(user_id):
+            return await query.answer("You're not allowed to perform this action!", show_alert=True)    
+        
+        text = """**━━SMS Gateway Commands━━**
+  
+**/spam | !spam** - Send as reply to leads file with message as caption
+**/test &lt;number&gt; | !test &lt;number&gt;** - Test bot working status
+**/myacc | !myacc** - Your details
+**/mycredits | !mycredits** - Check your credits
+
+
+You can use `{number}` in message to replace it with the phone number""" 
+        markup = InlineKeyboardMarkup([
+                [InlineKeyboardButton("Back", f"help_back")]
+            ]) 
+
+            
+    elif sp == "back":
+        text = "**Which commands would you like to check?**" 
+        markup=InlineKeyboardMarkup([
+                               [InlineKeyboardButton("☎️ SMS Gateway Commands", f"help_gateway")]])
+        
+    await query.edit_message_text(text, reply_markup=markup) 
+        
+        
+@Client.on_message(filters.command(["spam"]))
+async def ookk(_, m):
+    if not isPremium(user_id):
+        return await m.reply("You are not authorized!")
+    file_id = m.reply_to_message.document.file_id
+    filecaption = m.reply_to_message.caption
+    filereq = requests.get('https://api.telegram.org/bot'+BOT_TOKEN+'/getFile?file_id='+file_id)
+    filepath = filereq.json()['result']['file_path']
+    
+    filedl = requests.get('https://api.telegram.org/file/bot'+BOT_TOKEN+'/'+filepath)
+    dllines = filedl.text.splitlines()
+    with open('temp/leads'+str(m.from_user.id)+'.txt', 'w') as file:
+        for line in dllines:
+            file.write(line+'\n')
+    
+    with open('temp/msg'+str(m.from_user.id)+'.txt', 'w', encoding="utf-8") as file:
+            file.write(filecaption)
+            
+    await m.reply("**Are you Sure? **", 
+                  reply_markup=InlineKeyboardMarkup([
+                               [InlineKeyboardButton("Yes!", f"sure_yes1"),
+                               InlineKeyboardButton("No!", f"sure_no1")]]))
+
+@Client.on_message(filters.command(["test"]))
+async def ookk(_, m):
+    sp = m.text.split(None, 1)
+    user_id = m.from_user.id
+    
+    if not isPremium(user_id):
+        return await m.reply("You are not authorized!")
+    
+    if sp:
+        responsesms = await sendSMS(sp[1],"Test message by Spammer Bot")
+        await m.reply(f"""Sent SMS to {sp[1]} - {responsesms}
+
+**Spamming /test command will lead to BAN!**""")
+        for admin in ADMIN_IDS:
+            await Client.send_message(chat_id=admin, text=f"{m.from_user.id} Sent a test SMS to {sp[1]}", self=app)
+
+@Client.on_callback_query(filters.regex(r"^sure_.*"))
+async def cb(bot: Client, query: CallbackQuery):
+    data = query.data
+    sp = data.split("_")[1]
+    user_id = query.from_user.id
+    
+    
+    if sp == "yes1":
+        await query.message.edit("**Are you 100% Sure?**", 
+                  reply_markup=InlineKeyboardMarkup([
+                               [InlineKeyboardButton("No!", f"sure_no2"),
+                                InlineKeyboardButton("Yes I'm 100% sure", f"sure_yes2")]]))
+    
+    if sp == "no1" or sp == 'no2':
+        await query.message.edit("**Cancelled Process.**")
+        os.remove('temp/leads'+str(user_id)+'.txt')
+        os.remove('temp/msg'+str(user_id)+'.txt')
+        
+    if sp == "yes2":
+        await query.message.edit("**You'll be notified once the process is over**")
+        
+        file=open('temp/leads'+str(user_id)+'.txt',"r",encoding="utf8",errors="ignore")
+        numbers=file.readlines()
+        
+        file=open('temp/msg'+str(user_id)+'.txt',"r",encoding="utf8",errors="ignore")
+        messagebody=file.read()
+        
+        sentleads = 0
+        failedleads = 0
+        completedleads = 0
+        totalleads = len(numbers)
+        
+        for number in numbers:
+            smsresponse = await sendSMS(number,messagebody)
+            
+            if smsresponse == True:
+                sentleads+=1
+            else:
+                failedleads+=1
+            
+            completedleads+=1
+            
+        if completedleads == totalleads:
+            await query.message.edit(f"""**💠 Status - <u>SENT!</u> ✔️**
+    
+    
+__🔲 Progress = {completedleads}/{totalleads}
+
+🔳 Queued = {totalleads-completedleads}
+
+
+🟩 Sent = {sentleads}
+
+🟥 Failed = {failedleads}__""")
+            
+            
+@Client.on_message(filters.command(["upgrade"]))
+async def upgrade(client:Client, message:Message):
+    if not message.from_user.id in ADMIN_IDS:
+        return 
+    
+    sp = message.text.split(None, 1)
+    if len(sp) == 1:
+        return await message.reply("**Provide UserId And Credits! Format: UserId|Credits**", quote=True)  
+    
+    kek = await message.reply("**Adding...**", quote=True)
+
+    try:
+        user_id = sp[1].split("|")[0]
+        credits = sp[1].split("|")[1]
+    except:
+        return await kek.edit("**Provide UserId And Credits! Format: UserId|Credits**")
+    
+    
+    upgrade = addPremium(user_id,credits)
+    if upgrade != True:
+        txt = f"**Couldn't add user to database**"
+    else:
+        txt = f"**Added credits to user!**\n**User ID:** `{user_id}`\n**Message:** `Added to database`\n**Current Credits:** `"+getCredits(user_id)+f"`\n**Added credits:** `{credits}`"
+     
+
+    await kek.edit(txt)
+    
+    
+@Client.on_message(filters.command(["suspend"]))
+async def suspend(client:Client, message:Message):
+    if not message.from_user.id in ADMIN_IDS:
+        return 
+    
+    sp = message.text.split(None, 1)
+    if not sp:
+        return await message.reply("**Provide UserId! Format: UserId**", quote=True)  
+    
+    kek = await message.reply("**Adding...**", quote=True)
+
+    try:
+        user_id = sp[1].split("|")[0]
+    except:
+        return await kek.edit("**Provide UserId! Format: UserId**")
+    
+    try:
+        suspend = banPremium(user_id)
+
+        if suspend != True:
+            txt = f"**Couldn't ban user from database**"
+        else:
+            txt = f"**Banned premium user!**\n**User ID:** `{user_id}`"
+    except Exception as e:
+        txt = e    
+    
+
+    await kek.edit(txt)
